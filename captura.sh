c319ff3edb1bfcd12a59c866603fb4292545f43f
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-09-19 21:57:33 -0500

    Inicio del proyecto

---
+++ captura.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#v4l2-ctl --set-ctrl gamma=1
+#v412-ctl --set-ctrl exposure=10000
+#v4l2-ctl --set-ctrl brightness=127
+#fswebcam --no-banner -r 1280x1024 /home/elmaps/webcam/foto.png
+
+#Decimos donde guardaremos la foto
+path="/home/elmaps/plantita"
+cd ${path}
+
+#Horas investigando, noté que no podía hacer push desde crontab, pero descubrí que con esta línea ya me deja
+#https://chai-bapat.medium.com/how-to-automate-github-using-cron-16effc825bcf
+export GIT_SSH_COMMAND="ssh -i /root/.ssh/id_rsa_gh"
+
+#Antes de cualquier cosa, bajamos los cambios
+git pull
+
+#Obtenemos el número de la foto
+iter=$(cat step.txt)
+
+locacion="${path}/fotos/foto${iter}.jpg"
+
+#Tomamos la foto
+fswebcam --no-banner -r 176x144 --jpeg 100 -S 23 ${locacion}
+
+#Aumentamos el contador de fotos
+iter=$((iter+1))
+#Guardamos el contador en el archivo
+echo $iter > step.txt
+
+#Guardamos los cambios en el repositorio
+git add .
+git commit -m "Foto ${iter} tomada"
+git push origin master
+
+#Mandamos un aviso de que se tomó la foto
+sh alert.sh "Foto ${iter} tomada" "Foto tomada correctamente"
