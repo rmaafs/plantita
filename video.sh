8ba75d3f8b51449ba9e38d5fe8bcf5fb5ddd9c78
Rodrigo Maafs <rodrigoelmaps@gmail.com>
2021-10-31 20:17:56 -0600

    Primera animación creada

--- video.sh
+++ video.sh
@@ -1,2 +1,2 @@
 #Generar un gif con todas las fotos
-ffmpeg -f image2 -i /home/elmaps/plantita/fotos/foto%d.jpg final.gif
+ffmpeg -r 10 -f image2 -i /home/elmaps/plantita/fotos/foto%d.jpg final3.gif
